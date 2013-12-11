c3ef1417a1b0e2cfa925f2a552491cc3482c6fc7
jcbusche <jcbusche@gmail.com>
2013-12-10 21:59:34 -0500

    Forgot link to main.js

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
 	<div id = 'display'></div>
 
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
     
 </body>
 </html>
