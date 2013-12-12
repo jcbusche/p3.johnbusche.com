01c438a37cec8f2d8a98a6c7f93567467e145d73
jcbusche <jcbusche@gmail.com>
2013-12-11 23:12:15 -0500

    adding the rest of the functions

--- index.php
+++ index.php
@@ -21,22 +21,63 @@
 	<div id = 'title'>
 		<h1>John's Speed Reading Trainer</h1>
 		<p>This is an application to help you improve your reading speed by challenging yourself to read faster.  First, copy text from an article you want to read into the box below.  Then select your word chunk size and your chunk display rate.  These will determine how many words are displayed at a time and how fast the app will cycle through them. </p>
+    </div>
 
 	<div id = 'text-input' class = 'input'>
+        <form>
 		<label for = 'toread'>Place the text for your article here:</label><br>
 		<input type='textarea' id = 'toread'>
 		<br><br>
-
+        <label for = "chunksize">Words per chunk<label>
+        <select id = "chunksize">
+            <option value= 1 selected >1</option>
+            <option value= 2 > 2 </option>
+            <option value= 3 > 3 </option>
+            <option value= 4 > 4 </option>
+            <option value= 5 > 5 </option>
+            <option value= 6 > 6 </option>
+            <option value= 7 > 7 </option>
+            <option value= 8 > 8 </option>
+            <option value= 9 > 9 </option>
+            <option value= 10 > 10 </option>
+        </select>
+        <label for = "speed">Chunks per minute<label>
+        <select id = "speed">
+            <option value= 60 selected >60</option>
+            <option value= 70 > 70 </option>
+            <option value= 80 > 80 </option>
+            <option value= 90 > 90 </option>
+            <option value= 100 > 100 </option>
+            <option value= 110 > 110 </option>
+            <option value= 120 > 120 </option>
+            <option value= 130 > 130 </option>
+            <option value= 140 > 140 </option>
+            <option value= 150 > 150 </option>
+            <option value= 160 > 160 </option>
+            <option value= 170 > 170 </option>
+            <option value= 180 > 180 </option>
+            <option value= 190 > 190 </option>
+            <option value= 200 > 200 </option>
+            <option value= 210 > 210 </option>
+            <option value= 220 > 220 </option>
+            <option value= 230 > 230 </option>
+            <option value= 240 > 240 </option>
+            <option value= 250 > 250 </option>
+            <option value= 260 > 260 </option>
+        </select>
+            
 		<input type = 'button' id = 'display-btn' value = 'READ!'>
-	<div>
+        
+            
+    </div>
 
 	<div id = 'options' class = 'input'>
 
 	</div>
 
-	<div id = 'display'></div>
+	<div id = 'display'>Your text will appear here.</div>
 
-	<script type="text/javascript" src="js/main.js"></script>
+	
     
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 $('#display-btn').click(function(){
-	var array1 = $("#toread").val().split(' ');
-	var numwords = array1.length;
-	console.log(array1);
+	var words = $("#toread").val().split(' ');
+	var numwords = words.length;
+    var chunksize = $("#chunksize").val();
+    var cpm = $("#speed").val();
+    var counter = 0;
+    var counter2 = 0;
+    console.log(words);
+    console.log(numwords);
+    var chunks = [];
+    for (k = 0; k < numwords/chunksize ; k ++){
+        chunks[k] = "";
+    }
+    for (i = 0; i< numwords; i+=chunksize) {
+        var diff = numwords - i;
+        if (diff >= chunksize +1){
+            for (j = 0; j < chunksize; j++) {
+                chunks[counter2] = chunks[counter2]  + words[i + j] + " ";
+                console.log(chunks);
+            }
+        }
+        console.log(words);
+        else {
+            for (j = 0; j< diff; j++){
+                chunks[counter2] = chunks[counter2] + words[i + j] + " ";
+            }
+        }
+        counter2 = counter2 + 1;
+    }
+    console.log(chunks);
+    setInterval(function(){
+        $('#display').text(chunks[counter]);
+        counter = counter + 1 ;
+     }, 60000/cpm);
 });
+        
